ea331cddf44056e4c9f3e7e7e1998c4ea43d1566
Raman Batra <raman.batra1709@gmail.com>
2018-11-05 13:44:52 +0530

    Add easyhttp module

---
+++ src/http.js
@@ -0,0 +1,41 @@
+class EasyHTTP  {
+    async get(url)   {
+        const response = await fetch(url);
+        const responseData = await response.json();
+        return responseData;
+    }
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const responseData = await response.json();
+        return responseData;
+    }
+    async put(url, data)  {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const responseData = await response.json();
+        return responseData;
+    }
+    async delete(url)  {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+        const responseData = await 'Resource deleted';
+        return responseData;
+    }
+}
+
+export const http = new EasyHTTP();
