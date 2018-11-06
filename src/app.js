af83a27bdec634a24fd62972e4896ebeec633e8b
Raman Batra <raman.batra1709@gmail.com>
2018-11-06 13:59:06 +0530

    Add getposts handler

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 import { http } from './http';
+import { ui } from './ui';
+
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+    http.get('http://localhost:3000/posts')
+        .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}
